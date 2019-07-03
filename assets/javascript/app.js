523cca2dcf16124d2e1fe977e9041b410bbf06ad
Andrew Stiles <ndrwstls544@gmail.com>
2019-07-03 15:42:12 -0400

    added files

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+let intervalId;
+let questions = [
+    {
+        question: "Who is the strongest?",
+        answers: {
+          a: "Superman",
+          b: "The Terminator",
+          c: "Waluigi, obviously"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "What is the best site ever created?",
+        answers: {
+          a: "SitePoint",
+          b: "Simple Steps Code",
+          c: "Trick question; they're both the best"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Where is Waldo really?",
+        answers: {
+          a: "Antarctica",
+          b: "Exploring the Pacific Ocean",
+          c: "Sitting in a tree",
+          d: "Minding his own business, so stop asking"
+        },
+        correctAnswer: "d"
+    }
+]
+//references to html//
+let TimeRemaining = $("#time-remaining");
+let question = $("#question");
+let answer = $("#answers");
